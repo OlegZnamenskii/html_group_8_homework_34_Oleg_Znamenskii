6a03225eeb3adde4906fe882c43a9b678bfa4a5d
Oleg Znamenskii <555oleg666@gmail.com>
2017-03-07 12:50:44 +0600

    #5+#6 fixed responsive & design

--- js/main.js
+++ js/main.js
@@ -22,3 +22,17 @@ var validation = $('#reg-formp').validate({
 });
 
 $('#phone_p').mask("+(999) 999-9999");
+;
+
+//плавная прокрутка
+
+menu.on("click","a", function (event) {
+
+    event.preventDefault();
+
+    var id  = $(this).attr('href'),
+
+        top = $(id).offset().top;
+
+    $('body,html').animate({scrollTop: top}, 1000);
+});
